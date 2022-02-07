beac96269b9cd5c737549aa8da9aadb933b14925
DubbedTelegrambot <94349234+DubbedTelegrambot@users.noreply.github.com>
2022-02-07 19:42:39 +0530

    Update _run_.py

--- LuciferMoringstar_Robot/modules/_run_.py
+++ LuciferMoringstar_Robot/modules/_run_.py
@@ -1,6 +1,9 @@
 from pyrogram import Client as LuciferMoringstar_Robot, filters as Worker
 from LuciferMoringstar_Robot.modules.autofilter import group_filters, pm_autofilter
 from config import AUTH_GROUPS, AUTH_USERS
+from LuciferMoringstar_Robot.database.broadcast_db import Database
+
+db = Database()
 
 
 
@@ -12,6 +15,8 @@ async def groupfilters(client, message):
 
 @LuciferMoringstar_Robot.on_message(Worker.text & Worker.private & Worker.incoming & Worker.user(AUTH_USERS) if AUTH_USERS else Worker.text & Worker.private & Worker.incoming)
 async def pm_filters(client, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
     await pm_autofilter(client, message)
 
 
